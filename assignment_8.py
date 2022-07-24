1b9b3ecf251144ad6ede9abf2a746ada92014449
Shovan Mohapatra <53206197+shovan03@users.noreply.github.com>
2022-07-24 22:23:35 +0530

    Update assignment_8.py

--- assignment_8.py
+++ assignment_8.py
@@ -4,8 +4,9 @@ st.write("""
 # Addition of two numbers
 """)
 
-def user_input():
-    number_1 = st.number_input("First Number")
-    number_2 = st.number_input("Second Number")
+number_1 = st.number_input("First Number")
+number_2 = st.number_input("Second Number")
 
-    return number_1 + number_2
+sum = number_1 + number_2
+
+st.write(f"The sum is: {sum}")
